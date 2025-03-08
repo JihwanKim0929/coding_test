45b542d56c43fc17782045cc34a6955fe02f90f4
tazan <tazanboy2@naver.com>
2025-03-09 01:44:20 +0900

    .

--- src/2025_03_09.java
+++ src/2025_03_09.java
@@ -27,3 +27,26 @@
 //        System.out.println(answer*answer);
 //    }
 //}
+
+//import java.util.*;
+//import java.io.*;
+//class p92{
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int l = Integer.parseInt(st.nextToken());
+//        int r = Integer.parseInt(st.nextToken());
+//        long[][][] arr = new long[n+1][l+1][r+1];
+//        arr[1][1][1]=1;
+//        for(int i=2;i<=n;i++){
+//            //[n-1][l-1][r]+[n-1][l][r-1]+[n-1][l][r]*n-2
+//            for(int j=1;j<=l;j++){
+//                for(int k=1;k<=r;k++){
+//                    arr[i][j][k] = (arr[i-1][j-1][k]+arr[i-1][j][k-1]+arr[i-1][j][k]*(i-2))%1000000007;
+//                }
+//            }
+//        }
+//        System.out.println(arr[n][l][r]);
+//    }
+//}
