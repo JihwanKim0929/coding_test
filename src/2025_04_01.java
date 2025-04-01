d1ada50cf6092094f154f2e3a5e5f75ea93a3a67
tazan <tazanboy2@naver.com>
2025-04-01 18:55:28 +0900

    .

--- src/2025_04_01.java
+++ src/2025_04_01.java
@@ -66,3 +66,193 @@
 //        }
 //    }
 //}
+
+//import java.util.*;
+//import java.io.*;
+//class p61{
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        int n = Integer.parseInt(br.readLine());
+//        int e = Integer.parseInt(br.readLine());
+//        int[][] distance = new int[n+1][n+1];
+//        for(int i=1;i<=n;i++){
+//            Arrays.fill(distance[i],10000001);
+//            distance[i][i] = 0;
+//        }
+//        for(int i=0;i<e;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int orig = Integer.parseInt(st.nextToken());
+//            int dest = Integer.parseInt(st.nextToken());
+//            int val = Integer.parseInt(st.nextToken());
+//            if(val<distance[orig][dest])
+//                distance[orig][dest] = val;
+//        }
+//        for(int k=1;k<=n;k++){
+//            for(int i=1;i<=n;i++){
+//                for(int j=1;j<=n;j++){
+//                    if(distance[i][j]>distance[i][k]+distance[k][j]){
+//                        distance[i][j]=distance[i][k]+distance[k][j];
+//                    }
+//                }
+//            }
+//        }//i->j로 가는 최단거리가 한방에 확정되는게 아님. 모든 중간경로 다 고려해서 가장 짧은걸로 확정됨
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=n;j++){
+//                int ans = distance[i][j];
+//                if(ans==10000001)
+//                    System.out.print(0+" ");
+//                else System.out.print(ans+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+//import java.util.*;
+//import java.io.*;
+//class p62{
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        int n = Integer.parseInt(br.readLine());
+//        int[][] ans = new int[n][n];
+//        for(int i=0;i<n;i++){
+//            st = new StringTokenizer(br.readLine());
+//            for(int j=0;j<n;j++){
+//                ans[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        for(int k=0;k<n;k++){
+//            for(int i=0;i<n;i++){
+//                for(int j=0;j<n;j++){
+//                    if(ans[i][k]==1 && ans[k][j]==1)
+//                        ans[i][j]=1;
+//                }
+//            }
+//        }
+//
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n;j++){
+//                System.out.print(ans[i][j]+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+//import java.util.*;
+//import java.io.*;
+//class p63{
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int e = Integer.parseInt(st.nextToken());
+//        int[][] ans = new int[n+1][n+1];
+//        for(int i=1;i<=n;i++){
+//            Arrays.fill(ans[i],100000000);
+//            ans[i][i] = 0;
+//        }
+//        for(int i=0;i<e;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int orig = Integer.parseInt(st.nextToken());
+//            int dest = Integer.parseInt(st.nextToken());
+//            ans[orig][dest]=1;
+//            ans[dest][orig]=1;
+//        }
+//        for(int k=1;k<=n;k++){
+//            for(int i=1;i<=n;i++){
+//                for(int j=1;j<=n;j++){
+//                    if(ans[i][j]>ans[i][k]+ans[k][j]){
+//                        ans[i][j] = ans[i][k]+ans[k][j];
+//                    }
+//                }
+//            }
+//        }
+//        int[] sum = new int[n+1];
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=n;j++){
+//                sum[i]+=ans[i][j];
+//            }
+//        }
+//        int min = 1;
+//        for(int i=2;i<=n;i++){
+//            if(sum[i]<sum[min])
+//                min = i;
+//        }
+//        System.out.println(min);
+//    }
+//}
+
+//import java.util.*;
+//import java.io.*;
+//class p64{
+//    static int[] parent;
+//    public static void main(String[] args)throws IOException {
+//        //노드,엣지 수 입력 받기
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+//        int e = Integer.parseInt(st.nextToken());
+//        //엣지 리스트 생성(우선순위 큐)
+//        PriorityQueue<Edge> edgeList = new PriorityQueue<>();
+//        //엣지 입력 받기
+//        for(int i=0;i<e;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int o = Integer.parseInt(st.nextToken());
+//            int d = Integer.parseInt(st.nextToken());
+//            int v = Integer.parseInt(st.nextToken());
+//            edgeList.add(new Edge(o,d,v));
+//        }
+//        //유니온 파인드 배열 생성
+//        parent = new int[n+1];
+//        for(int i=1;i<=n;i++)
+//            parent[i] = i;
+//        //총합, 선택 엣지 수 변수 생성
+//        int sum = 0 , selected = 0;
+//        //엣지들 하나씩 꺼내서 find해서 사이클 생성 안되면 유니온
+//        //n-1이면 종료
+//        while(selected<n-1){
+//            Edge now = edgeList.poll();
+//            if(find(now.orig)!=find(now.dest)){
+//                union(now.orig,now.dest);
+//                sum+=now.val;
+//                selected++;
+//            }
+//        }
+//        System.out.println(sum);
+//    }
+//    static int find(int n){
+//        if(parent[n] == n){
+//            return n;
+//        }
+//        return parent[n] = find(parent[n]);
+//    }
+//
+//    static void union(int a, int b){
+//        a = find(a);
+//        b = find(b);
+//        if(a!=b){
+//            parent[b] = a;
+//        }
+//    }
+//}
+//
+//class Edge implements Comparable<Edge>{
+//
+//    int orig;
+//    int dest;
+//    int val;
+//    Edge(int orig, int dest, int val){
+//        this.orig = orig;
+//        this.dest = dest;
+//        this.val = val;
+//    }
+//
+//    @Override
+//    public int compareTo(Edge o) {
+//        return this.val-o.val;
+//    }
+//}
