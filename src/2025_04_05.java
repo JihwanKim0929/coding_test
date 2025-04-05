8f2a4f88a721c5c5239c4930376ab966ad913dd9
tazan <tazanboy2@naver.com>
2025-04-05 21:24:30 +0900

    .

--- src/2025_04_05.java
+++ src/2025_04_05.java
@@ -139,3 +139,162 @@
 //        }
 //    }
 //}
+
+//import java.util.*;
+//import java.io.*;
+//
+//class p74{
+//    static boolean[] visited;
+//    static ArrayList<Integer>[] graph;
+//    static int[] parent;
+//    static int[] depth;
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        int n = Integer.parseInt(br.readLine());
+//        graph = new ArrayList[n+1];
+//        for(int i=1;i<=n;i++)
+//            graph[i] = new ArrayList<>();
+//        depth = new int[n+1];
+//        parent = new int[n+1];
+//        visited = new boolean[n+1];
+//        for(int i=0;i<n-1;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            graph[a].add(b);
+//            graph[b].add(a);
+//        }
+//        depth[1] = 0;
+//        dfs(1);
+//
+//        //질문 받기
+//        int q = Integer.parseInt(br.readLine());
+//        for(int i=0;i<q;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            System.out.println(getLCA(a,b));
+//        }
+//    }
+//    static void dfs(int n){
+//        if(visited[n])
+//            return;
+//        visited[n]=true;
+//        for(int i:graph[n]){
+//            if(!visited[i]){
+//                depth[i] = depth[n]+1;
+//                parent[i] = n;
+//                dfs(i);
+//            }
+//        }
+//    }
+//
+//    static int getLCA(int a, int b){
+//        //a가 깊이가 더 작도록 만듦
+//        if(depth[a]>depth[b]){
+//            int tmp = b;
+//            b = a;
+//            a = tmp;
+//        }
+//        //b가 a의 깊이가 되도록 올림
+//        while(depth[b]>depth[a]){
+//            b = parent[b];
+//            if(a==b)
+//                return a;
+//        }
+//        while(true){
+//            if(a==b)
+//                return a;
+//            a = parent[a];
+//            b = parent[b];
+//        }
+//    }
+//}
+
+//import java.util.*;
+//import java.io.*;
+//class p75{
+//    static ArrayList<Integer>[] graph;
+//    static int[] depth;
+//    static int[][] p;
+//    static int maxK;
+//    static boolean[] visited;
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int n = Integer.parseInt(br.readLine());
+//        graph = new ArrayList[n+1];
+//        for(int i=1;i<=n;i++)
+//            graph[i] = new ArrayList<>();
+//
+//        StringTokenizer st;
+//        for(int i=0;i<n-1;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            graph[a].add(b);
+//            graph[b].add(a);
+//        }
+//        int tmp = 1;
+//        maxK=0;
+//        while(tmp<n){
+//            tmp*=2;
+//            maxK++;
+//        }
+//        depth = new int[n+1];
+//        p = new int[maxK+1][n+1];
+//
+//        depth[1] = 0;
+//        visited = new boolean[n+1];
+//        dfs(1);
+//
+//        for(int k=1;k<=maxK;k++){
+//            for(int i=1;i<=n;i++){
+//                p[k][i] = p[k-1][p[k-1][i]];
+//            }
+//        }
+//
+//        int q = Integer.parseInt(br.readLine());
+//        for(int i=0;i<q;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            System.out.println(getLCA(a,b));
+//        }
+//    }
+//    static void dfs(int n){
+//        if(visited[n])
+//            return;
+//        visited[n]=true;
+//        for(int i:graph[n]){
+//            if(!visited[i]){
+//                depth[i]=depth[n]+1;
+//                p[0][i] = n;
+//                dfs(i);
+//            }
+//        }
+//    }
+//    static int getLCA(int a, int b){
+//        if(depth[a]>depth[b]){
+//            int tmp = b;
+//            b = a;
+//            a = tmp;
+//        }
+//        //높이 맞추기
+//        for(int i = maxK;i>=0;i--){
+//            if(depth[b]-depth[a]>=Math.pow(2,i)){
+//                b = p[i][b];
+//            }
+//        }
+//        //찾기
+//        for(int k = maxK;k>=0;k--){
+//            if(p[k][a]!=p[k][b]){
+//                a = p[k][a];
+//                b = p[k][b];
+//            }
+//        }
+//        if(a!=b)
+//            return p[0][a];
+//        return a;
+//    }
+//}
