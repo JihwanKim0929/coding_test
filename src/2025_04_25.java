87dde550823b2c65484659c3a9d2ff8dc5f3ffe8
tazan <tazanboy2@naver.com>
2025-04-25 03:16:20 +0900

    .

---
+++ src/2025_04_25.java
@@ -0,0 +1,33 @@
+//import java.util.*;
+//import java.io.*;
+//
+//class p94{
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        int n = Integer.parseInt(br.readLine());
+//        int[][] arr = new int[n][2];
+//        for(int i=0;i<n;i++){
+//            st = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(st.nextToken());
+//            int b = Integer.parseInt(st.nextToken());
+//            arr[i][0] = a;
+//            arr[i][1] = b;
+//        }
+//
+//        int[][] dp = new int[n][n];
+//        for(int i=0;i<n;i++) {
+//            Arrays.fill(dp[i], Integer.MAX_VALUE);
+//            dp[i][i] = 0;
+//        }
+//        for(int i=2;i<=n;i++){
+//            for(int j=0;j<=n-i;j++){
+//                for(int k = j+1;k<=j+i-1;k++){
+//                    dp[j][j+i-1] = Math.min(dp[j][j+i-1],dp[j][k-1]+dp[k][j+i-1]+arr[j][0]*arr[k][0]*arr[j+i-1][1]);
+//                }
+//            }
+//        }
+//
+//        System.out.println(dp[0][n-1]);
+//    }
+//}
