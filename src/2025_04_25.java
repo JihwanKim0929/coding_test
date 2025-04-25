3f3be7fe5ebdce62376568ed8bc0370774d1c288
tazan <tazanboy2@naver.com>
2025-04-25 17:31:57 +0900

    .

--- src/2025_04_25.java
+++ src/2025_04_25.java
@@ -31,3 +31,43 @@
 //        System.out.println(dp[0][n-1]);
 //    }
 //}
+
+//import java.util.*;
+//import java.io.*;
+//class Main{
+//    static int n;
+//    static int[][] w;
+//    static int[][] d;
+//    static int inf = 1000000*16+1;
+//    public static void main(String[] args)throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        n = Integer.parseInt(br.readLine());
+//        w = new int[n][n];
+//        StringTokenizer st;
+//        for(int i=0;i<n;i++){
+//            st = new StringTokenizer(br.readLine());
+//            for(int j=0;j<n;j++){
+//                w[i][j] = Integer.parseInt(st.nextToken());
+//            }
+//        }
+//
+//        d = new int[n][1<<n];
+//        System.out.println(tsp(0,1));
+//    }
+//
+//    static int tsp(int c, int v){
+//        if(v==(1<<n)-1){
+//            return w[c][0]==0? inf:w[c][0];
+//        }
+//        if(d[c][v]!=0){
+//            return d[c][v];
+//        }
+//        d[c][v] = inf;
+//        for(int i=0;i<n;i++){
+//            if((v&(1<<i))==0 && w[c][i]!=0){
+//                d[c][v] = Math.min(d[c][v],tsp(i,(v|(1<<i)))+w[c][i]);
+//            }
+//        }
+//        return d[c][v];
+//    }
+//}
